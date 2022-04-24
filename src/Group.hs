d32062d299d05e6610912620bf2825419d539818
Quantumzhao <franklinzhao123@hotmail.com>
2022-04-24 15:36:27 -0400

    removed unnecessary filees

--- src/Group.hs
+++
@@ -1,7 +0,0 @@
-module Group where
-
-import Set
-
--- data Group f = Group Set f
-
-
--- src/Unit.hs
+++
@@ -1,8 +0,0 @@
-module Unit where
-
-import Node
-import ContextState
-
--- genUnit = Object Definition unitTags
-
-unitTags = ["Unit"]
